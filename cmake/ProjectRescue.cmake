448c4b2b9f3ef207c3b3f34110ea5927016263ad
winderica <winderica@gmail.com>
2021-09-20 02:52:49 +0000

    Fix: fix rescue

--- cmake/ProjectRescue.cmake
+++ cmake/ProjectRescue.cmake
@@ -4,12 +4,14 @@ ExternalProject_Add(rescue
     DOWNLOAD_COMMAND ""
     CONFIGURE_COMMAND ""
     INSTALL_COMMAND ""
-    BUILD_COMMAND cargo build --release
-    COMMAND cargo build --release
+    BUILD_COMMAND ${CMAKE_COMMAND} -E env CARGO_TARGET_DIR=${CMAKE_SOURCE_DIR}/deps/rescue cargo build --release
+    COMMAND ${CMAKE_COMMAND} -E env CARGO_TARGET_DIR=${CMAKE_SOURCE_DIR}/deps/rescue cargo build --release
     BINARY_DIR "${CMAKE_SOURCE_DIR}/rust"
+    BUILD_ALWAYS TRUE
     LOG_BUILD 1
 )
 
 add_library(RESCUE STATIC IMPORTED)
-set(RESCUE_LIBRARY ${CMAKE_SOURCE_DIR}/rust/target/release/${CMAKE_STATIC_LIBRARY_PREFIX}rescue${CMAKE_STATIC_LIBRARY_SUFFIX})
+add_dependencies(RESCUE rescue)
+set(RESCUE_LIBRARY ${CMAKE_SOURCE_DIR}/deps/rescue/release/${CMAKE_STATIC_LIBRARY_PREFIX}rescue${CMAKE_STATIC_LIBRARY_SUFFIX})
 set_property(TARGET RESCUE PROPERTY IMPORTED_LOCATION ${RESCUE_LIBRARY})
